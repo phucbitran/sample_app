0c9ccec27812a2274497fdefa3368bce1c030ce1
TTPhuc <phucbitran@gmail.com>
2018-06-06 08:09:01 +0700

    chapter5

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save{email.downcase!}
+  validates :name, presence: true, lenght: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, lenght: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
+  has_secure_password
+end
